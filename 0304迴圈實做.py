3bb8720a00e3fe3c0b70b665368aa849300fadaf
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-04 21:31:24 +0800

    Add files via upload

---
+++ 0304迴圈實做.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 21:05:12 2021
+
+@author: user
+"""
+#sum=0
+#for i in range(10):
+#    sum=sum + i
+ #   print(sum)
+#print('完畢')
+
+
+
+for i in range(10):
+    if i %2 ==0:
+        print(i,'是偶數')
+    
+print('完畢')
+
+
+
+
+
+
