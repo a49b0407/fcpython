f7cb130517b7994313450c71360d82263e0b6e38
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-04 21:48:01 +0800

    Add files via upload

---
+++ 0304加總.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 21:43:20 2021
+
+@author: user
+"""
+
+total=0
+num = int(input('輸入數字:'))
+for a in range(1,num+1):
+    total +=a
+print('總和',total)
+
+
