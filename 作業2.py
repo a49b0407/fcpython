e670ed0f3e3ec91c41bb27b577e7ce3927847899
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-05 11:03:14 +0800

    Add files via upload

--- 作業2.py
+++ 作業2.py
@@ -6,18 +6,13 @@ Created on Fri Mar  5 00:25:04 2021
 """
 
  #第二題用range印出 (2,4,6,8)
-a=0
-b=0
-c=0
-d=0
 for i in range(1,9):
-     if i/2==1:
-      i==a
-     elif i/4==1:
-      i==b
-     elif i/6==1:
-      i==c
-     elif i/8==1:
-      i==d   
-      print(a,b,c,d)
+   if i/2==1:
+       for a in range(1,9):
+          if a/4==1:
+             for b in range(1,9):
+                if b/6==1:
+                  for c in range(1,9): 
+                      if c/8==1:
+                       print(i,a,b,c)
    
