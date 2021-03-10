8fb5eac75f19f07c4e4ebd81f1cc87cce6f0ed1c
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-10 11:38:42 +0800

    Add files via upload

---
+++ 0309作業一.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 11:00:47 2021
+
+@author: fb-5231
+"""
+#印出
+#123456789
+#12345678
+#1234657
+#123456
+#12345
+#1234
+#123
+#123
+#12
+#1
+for i in range(10,0,-1):
+    for x in range(1,i):
+    #    print(x,end='')
+    print('*')
