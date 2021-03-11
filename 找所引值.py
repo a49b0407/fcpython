10cfbe4adc554769cb24d3cd5f69404d2bd6de58
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-11 19:38:50 +0800

    Add files via upload

---
+++ 找所引值.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 18:47:05 2021
+
+@author: user
+"""
+
+student=['john','bill','cherry','david']
+ind=student.index('bill')
+print(ind)
+#找值得索引值
+
+
+
