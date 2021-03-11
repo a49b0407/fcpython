51e7afa5574c47a480f2ea0ae8a50483d7165d54
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-11 19:39:39 +0800

    Add files via upload

---
+++ len.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 18:47:03 2021
+
+@author: user
+"""
+data = [100,2,3,4,5,'dog']
+for i in range(len(data)):
+    print(data[i])
+    
+    
+for d in data: #把data的質疑個個抓出來給d
+    print(d)
+    
