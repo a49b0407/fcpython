b3d628cd9745c13ab23c9429c30c39728f66e39c
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-11 19:39:16 +0800

    Add files via upload

---
+++ 抓直.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 18:47:05 2021
+
+@author: user
+"""
+
+number=[10,20,30,40,50,60,70]#抓前3項
+print('前一項:',number[:3])
+print('抓所飲職2之後:',number[2:])
+print('抓所飲職1-4',number[1:5])
+print('往前',number[-3:7])
