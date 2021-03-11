06c2c568d40b960e5835af3da530f14e5fa15a96
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-11 21:48:20 +0800

    Add files via upload

--- 求列大小.py
+++ 求列大小.py
@@ -4,11 +4,11 @@ Created on Thu Mar 11 21:37:57 2021
 
 @author: user
 """
-num=[30,60,1,20]
+num=[30,60,1,20,25,30,22,50,105]
 total=sum(num)
 ma=max(num)
 mc=min(num)
 print(ma,mc,total)
 
-
-
+s=max(num[2:6])
+print(s)
