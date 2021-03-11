8215a7d52f4f78075fd2615ede982efdae07282b
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-11 20:31:26 +0800

    Add files via upload

---
+++ 輸入串列.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 20:20:11 2021
+
+@author: user
+"""
+student=[]
+while True :
+    st=input('輸入學生名(q)離開')
+    if st=='q':
+        break
+    student.append(st)
+print(student)
+#input 輸入至串列
+
+
