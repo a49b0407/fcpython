b3d5be4e4b2b9e4bbd71dda67545f7abfb437f8e
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-16 19:45:24 +0800

    Add files via upload

---
+++ 星印1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 19:16:49 2021
+
+@author: user
+"""
+
+for i in range(1,5):
+    print(' '*(4-i)+'*'*(2*i-1))
+          
+for i in range(3,0,-1):
+        
+    print(' '*(4-i)+'*'*(2*i-1))
