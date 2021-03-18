e422f16e23b5c832c46dce1c2e1dd8325512d998
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-18 21:52:25 +0800

    Add files via upload

--- 格式碼.py
+++ 格式碼.py
@@ -8,5 +8,12 @@ name='peter'
 pi=3.1415926
 money=10000
 print('學生:%s的存款%d元'%(name,money))
-print('存款:%10d,目前利率:%10.21f'%(money,pi))
+print('存款:%10d,目前利率:%10.2f'%(money,pi))
+print('姓名:{},存款為{}元'.format(name,money))
+print('利率:{0},金額:{1},pi:{0}'.format(pi,money))
+print('存款:{:,},利率:{:.2f}'.format(money,pi))
+print('存款:{0:10d},補0:{0:0>10d}'.format(money))
+print('姓名:{:>10s}君'.format(name))
+print('姓名:{:<10s}君'.format(name))
+
 
