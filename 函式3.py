554596b00a795d34cc4284db0f78d0f215471856
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-03-23 21:54:47 +0800

    Add files via upload

---
+++ 函式3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 21:39:23 2021
+
+@author: user
+"""
+def personal(school='聯成',city='台中市'):
+ print('學校:',school)
+ print('城市:',city)
+personal()
+personal('逢甲','台北市')#位置參數
+personal('中興')
+personal(city='高雄市',school='中山大')
+personal(city='台南市')#關鍵字函式
+
+
+
