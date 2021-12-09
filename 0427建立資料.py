5c1ba672c33ba78e14e00c3a1d58f9b39d205171
a49b0407 <79953213+a49b0407@users.noreply.github.com>
2021-12-09 18:50:06 +0800

    Update 0427建立資料.py

--- 0427建立資料.py
+++ 0427建立資料.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('lcc.db')
 sql = "insert into customer(name,sex) values('王曉明','男'),('陳小利','女'),('陳比爾','男')"
 conn.execute(sql)#執行語法
 conn.commit()
-
+#https://docs.python.org/zh-tw/3/glossary.html#term-iterator
 
 
 sql = "insert into orders(prices,cid) values(10000,1),(1999,1),(3000,3),(8999,3)"
